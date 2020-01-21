f7f631b21776d2742e34b45a0832409d30bb918e
Hale Shaw <hongliang.xiao@sap.com>
2020-01-21 10:39:52 +0800

    Resolve http get return 403

--- src/main/java/io/wherein/token/utils/HttpClientUtils.java
+++ src/main/java/io/wherein/token/utils/HttpClientUtils.java
@@ -35,6 +35,8 @@ public class HttpClientUtils {
     try {
       URIBuilder uriBuilder = new URIBuilder(url);
       HttpGet httpGet = new HttpGet(uriBuilder.build());
+      httpGet.setHeader("User-Agent",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36");
       RequestConfig.Builder requestConfig = RequestConfig.custom();
       httpClient = HttpClients.custom().setDefaultRequestConfig(requestConfig.build()).build();
 
